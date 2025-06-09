de3bc57f9e1f9ac0f214ef2854fba737b33a8c6c
Federico D'Asaro <federico.dasaro@linksfoundation.com>
2025-06-09 18:20:47 +0200

    Add whisper base adapter, prompt, whisper medium and large frozen configs

---
+++ experiments_configs/recanvo/whisper_medium_frozen.py
@@ -0,0 +1,32 @@
+_base_ = [
+    '../_base_/base_model.py',
+    '../_base_/base_preprocessing.py',
+    '../_base_/base_training.py',
+]
+
+LABELS = [
+    'selftalk', 'frustrated', 'delighted', 'dysregulated', 'social', 'request'
+]
+
+model_config = dict(
+    model_type="whisper",
+    audio_model_name="openai/whisper-medium",
+    finetune_method="frozen",
+    num_labels=len(LABELS),
+    class_weights=None,
+    label2id={label: idx for idx, label in enumerate(LABELS)},
+    id2label={idx: label for idx, label in enumerate(LABELS)}
+)
+
+preprocessing_config=dict(
+    datasets_path="recanvo/",
+    audio_dataset_path="samples/",
+    dataset_name="recanvo",
+    label2id={label: idx for idx, label in enumerate(LABELS)},
+    audio_model_name="openai/whisper-medium",
+)
+
+training_config=dict(
+    output_model_name="recanvo_whisper_medium_frozen",
+    experiment_dir="recanvo/whisper_medium_frozen",
+)
