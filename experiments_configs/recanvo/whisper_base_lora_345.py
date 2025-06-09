33cb1d67290472576586d6ff446ca415b4cff4ef
Federico D'Asaro <federico.dasaro@linksfoundation.com>
2025-06-09 09:54:52 +0200

    Add config and jobs of partial LoRa experiments on Whisper base

---
+++ experiments_configs/recanvo/whisper_base_lora_345.py
@@ -0,0 +1,33 @@
+_base_ = [
+    '../_base_/base_model.py',
+    '../_base_/base_preprocessing.py',
+    '../_base_/base_training.py',
+]
+
+LABELS = [
+    'selftalk', 'frustrated', 'delighted', 'dysregulated', 'social', 'request'
+]
+
+model_config = dict(
+    model_type="whisper",
+    audio_model_name="openai/whisper-base",
+    finetune_method="lora",
+    apply_adapter_to_layers=[3,4,5],
+    num_labels=len(LABELS),
+    class_weights=None,
+    label2id={label: idx for idx, label in enumerate(LABELS)},
+    id2label={idx: label for idx, label in enumerate(LABELS)}
+)
+
+preprocessing_config=dict(
+    datasets_path="recanvo/",
+    audio_dataset_path="samples/",
+    dataset_name="recanvo",
+    label2id={label: idx for idx, label in enumerate(LABELS)},
+    audio_model_name="openai/whisper-base",
+)
+
+training_config=dict(
+    output_model_name="recanvo_whisper_base_lora_345",
+    experiment_dir="recanvo/whisper_base_lora_345",
+)
---
+++ experiments_configs/vivae/whisper_base_lora_012.py
@@ -0,0 +1,50 @@
+_base_ = [
+    '../_base_/base_model.py',
+    '../_base_/base_preprocessing.py',
+    '../_base_/base_training.py',
+]
+
+model_config = dict(
+    model_type="whisper",
+    audio_model_name="openai/whisper-base",
+    finetune_method="lora",
+    apply_adapter_to_layers=[0,1,2],
+    num_labels=6,
+    class_weights=None,
+    label2id={
+        "achievement": 0,
+        "anger": 1,
+        "fear": 2,
+        "pain": 3,
+        "pleasure": 4,
+        "surprise": 5
+    },
+    id2label={
+        0: "achievement",
+        1: "anger",
+        2: "fear",
+        3: "pain",
+        4: "pleasure",
+        5: "surprise"
+    }
+)
+
+preprocessing_config=dict(
+    datasets_path="vivae/",
+    audio_dataset_path="samples/",
+    dataset_name="vivae",
+    label2id={
+        "achievement": 0,
+        "anger": 1,
+        "fear": 2,
+        "pain": 3,
+        "pleasure": 4,
+        "surprise": 5
+    },
+    audio_model_name="openai/whisper-base",
+)
+
+training_config=dict(
+    output_model_name="vivae_whisper_base_lora_012",
+    experiment_dir="vivae/whisper_base_lora_012",
+)
