c0afb89743ffc9df0fd0b4e76c730fe6861f3327
Federico D'Asaro <federico.dasaro@linksfoundation.com>
2025-06-10 14:43:29 +0200

    Add Unispeech base configs

---
+++ experiments_configs/donateacry/unispeech_base_plus_frozen.py
@@ -0,0 +1,46 @@
+_base_ = [
+    '../_base_/base_model.py',
+    '../_base_/base_preprocessing.py',
+    '../_base_/base_training.py',
+]
+
+model_config = dict(
+    model_type="wav2vec2",
+    audio_model_name="microsoft/unispeech-sat-base-plus",
+    finetune_method="frozen",
+    num_labels=5,
+    class_weights=None,
+    label2id={
+        "belly_pain": 0,
+        "burping": 1,
+        "discomfort": 2,
+        "hungry": 3,
+        "tired": 4,
+    },
+    id2label={
+        0:"belly_pain",
+        1:"burping",
+        2:"discomfort",
+        3:"hungry",
+        4:"tired",
+    }
+)
+
+preprocessing_config=dict(
+    datasets_path="donateacry_corpus/",
+    audio_dataset_path="samples/",
+    dataset_name="donateacry",
+    label2id={
+        "belly_pain": 0,
+        "burping": 1,
+        "discomfort": 2,
+        "hungry": 3,
+        "tired": 4,
+    },
+    audio_model_name="microsoft/unispeech-sat-base-plus",
+)
+
+training_config=dict(
+    output_model_name="donateacry_unispeech_base_plus_frozen",
+    experiment_dir="donateacry/unispeech_base_plus_frozen",
+)
---
+++ experiments_configs/recanvo/unispeech_base_plus_frozen.py
@@ -0,0 +1,32 @@
+_base_ = [
+    '../_base_/base_model.py',
+    '../_base_/base_preprocessing.py',
+    '../_base_/base_training.py',
+]
+
+LABELS = [
+    'selftalk', 'frustrated', 'delighted', 'dysregulated', 'social', 'request'
+]
+
+model_config = dict(
+    model_type="wav2vec2",
+    audio_model_name="microsoft/unispeech-sat-base-plus",
+    finetune_method="frozen",
+    num_labels=len(LABELS),
+    class_weights=None,
+    label2id={label: idx for idx, label in enumerate(LABELS)},
+    id2label={idx: label for idx, label in enumerate(LABELS)}
+)
+
+preprocessing_config=dict(
+    datasets_path="recanvo/",
+    audio_dataset_path="samples/",
+    dataset_name="recanvo",
+    label2id={label: idx for idx, label in enumerate(LABELS)},
+    audio_model_name="microsoft/unispeech-sat-base-plus",
+)
+
+training_config=dict(
+    output_model_name="recanvo_unispeech_base_plus_frozen",
+    experiment_dir="recanvo/unispeech_base_plus_frozen",
+)
